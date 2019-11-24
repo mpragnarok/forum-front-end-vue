0aa243259cb107452eb2b6c92838e88ef169a287
mina <mp922352612@gmail.com>
2019-11-24 13:53:17 +0800

    Add Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="restaurant-forum">
+      <Navbar />
+      <main role="main" class="mt-5 bg-white">
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -23,6 +25,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
+import SignUp from '../views/SignUp.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: SignUp
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router

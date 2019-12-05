4cc579060176e04f587c6e71756ceb0871db1a30
mina <mp922352612@gmail.com>
2019-12-05 16:45:40 +0800

    Add TopRestaurantCard, RestaurantDashboard components and RestaurantsTop view

---
+++ src/components/TopRestaurantCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    class="card mb-3"
+    style="max-width: 540px;margin: auto;"
+  >
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <a href="#">
+          <img
+            class="card-img"
+            :src="restaurant.image"
+          />
+        </a>
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{restaurant.name}}</h5>
+          <span class="badge badge-secondary">Favorited: {{restaurant.FavoriteCount}}</span>
+          <p class="card-text">{{restaurant.description}}</p>
+          <router-link
+            class="btn btn-info mr-2"
+            :to="{name:'restaurant-dashboard',params: {id:restaurant.id}}"
+          >Show</router-link>
+
+          <button
+            type="button"
+            class="btn btn-danger mr-2"
+            @click.stop.prevent="addFavorite"
+          >移除最愛</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click.stop.prevent="deleteFavorite"
+          >加到最愛</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialRestaurant: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      restaurant: this.initialRestaurant
+    }
+  },
+  methods: {
+    addFavorite() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: true
+      }
+    },
+    deleteFavorite() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: false
+      }
+    }
+  }
+}
+</script>

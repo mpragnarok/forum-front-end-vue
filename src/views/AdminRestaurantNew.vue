b46d2fa2c631fcbc03b73f8505ab14d44ab467c9
mina <mp922352612@gmail.com>
2019-12-09 11:50:01 +0800

    Add AdminRestaurantNew API integration

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="container py-5">
-    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+    <AdminRestaurantForm
+      :is-processing="isProcessing"
+      @after-submit="handleAfterSubmit"
+    />
   </div>
 </template>
 
 <script>
 import AdminRestaurantForm from './../components/AdminRestaurantForm.vue'
+import adminAPI from '../apis/admin'
+import { Toast } from '../utils/helpers'
 
 export default {
   components: {
     AdminRestaurantForm
   },
+  data() {
+    return {
+      isProcessing: false
+    }
+  },
   methods: {
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ': ' + value)
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true
+        // 透過 restaurants.create 方法來向伺服器建立餐廳
+        const { data, statusText } = await adminAPI.restaurants.create({
+          formData
+        })
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
+        }
+        // 成功的話則轉址到 `/admin/restaurants`
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: "Can't create restaurant"
+        })
       }
     }
   }
